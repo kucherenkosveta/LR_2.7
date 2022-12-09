1830c7e3ea4e6edf0c31e0c96e294c8133efe971
kucherenkosveta <kucherenko55sss55@gmail.com>
2022-12-10 02:05:27 +0300

    /

---
+++ PyCharm/ind2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    s1 = set(input("Enter line №1: "))
+    s2 = set(input("Enter line №2: "))
+    line = ', '.join(s1.intersection(s2))
+    print(f"common characters in two lines: {line}")
